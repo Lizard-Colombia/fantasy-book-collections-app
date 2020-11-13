405c0539355540738547f1ba8ef557b78b11a10e
Lizard-Colombia <71155299+Lizard-Colombia@users.noreply.github.com>
2020-11-13 17:51:07 -0600

    deploy

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,15 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCF7voEzYod_CBC-ysyXrBgxGQInvhTCPU",
-  authDomain: "fantasy-book-collection.firebaseapp.com",
-  databaseURL: "https://fantasy-book-collection.firebaseio.com",
-  projectId: "fantasy-book-collection",
-  storageBucket: "fantasy-book-collection.appspot.com",
-  messagingSenderId: "1040213547901",
-  appId: "1:1040213547901:web:9e9e3c99fab7c80c15ca14",
-  measurementId: "G-SCPFJYKB0T"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
